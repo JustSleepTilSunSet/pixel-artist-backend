222b1bc8e5292417df3029b4da38c96a742edf7e
JustSleepTilSunSet <cloverchen.042@gmail.com>
2024-05-22 22:20:46 +0800

    Feat,containerize python service.

--- app.py
+++ app.py
@@ -1,5 +1,13 @@
 from flask import Flask
+from controllers.homeController import home
+
 app = Flask(__name__)
+app.add_url_rule('/home', 'home', home, methods=['GET'])
+
 @app.route("/")
-def hello():
-    return "hola!"
+def hello_world():
+    return "hola"
+
+if __name__ == '__main__':
+    app.run(port=10000)
+    # app.run(debug=True)// To cancel debug mode for external to connect the service.
