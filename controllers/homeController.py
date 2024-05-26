678361694db55c676c5a0fd2959beab801a9e275
JustSleepTilSunSet <cloverchen.042@gmail.com>
2024-05-26 16:34:36 +0800

    Feat,to add upload image api into service.

--- controllers/homeController.py
+++ controllers/homeController.py
@@ -1,4 +1,8 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify,request
 
 def home():
     return jsonify({'message': 'hola'})
+
+def saveImage():
+    print("Received data:", request.json)
+    return jsonify({'message': 'Hello, world!'}), 200
