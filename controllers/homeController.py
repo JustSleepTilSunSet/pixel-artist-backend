55e7eb5e4c81a418529fa3f9d31d138b492791d8
JustSleepTilSunSet <cloverchen.042@gmail.com>
2024-05-30 00:02:01 +0800

    Feat,add constants.

--- controllers/homeController.py
+++ controllers/homeController.py
@@ -1,8 +1,8 @@
 from flask import Flask, jsonify,request
-
+from model.Constants import status
 def home():
     return jsonify({'message': 'hola'})
 
 def saveImage():
     print("Received data:", request.json)
-    return jsonify({'message': 'Hello, world!'}), 200
+    return jsonify({'status': status["SUCCESS"], 'message': 'Hello, world!'})
---
+++ model/Constants.py
@@ -0,0 +1,4 @@
+status = {
+    "SUCCESS": 0,
+    "FAIL": -1
+}
