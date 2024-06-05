711d3ee4d1298f349124dd3d2737012badadec3f
JustSleepTilSunSet <cloverchen.042@gmail.com>
2024-06-05 23:59:01 +0800

    Feat,add create bucket.

--- model/clients/gcsClient.py
+++ model/clients/gcsClient.py
@@ -1,7 +1,7 @@
 from google.auth.credentials import AnonymousCredentials
 from google.cloud import storage
 conn = "http://fake-gcs-server:4443"
-
+bucket_name = "pixel-artist-dev-bucket"
 
 client = storage.Client(
     credentials=AnonymousCredentials(),
@@ -9,6 +9,13 @@ client = storage.Client(
     client_options={"api_endpoint": conn},
 )
 print(f"Connection with gcs established.");
+
 def listBuckets():
     for bucket in client.list_buckets():
         print(f"Bucket: {bucket.name}\n")
+
+def createBucket():
+
+    bucket = client.create_bucket(bucket_name)
+
+    print(f"Bucket {bucket.name} created")
