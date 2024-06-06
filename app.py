c7917e8579adea2b8136ecb38acc35a738acf98d
JustSleepTilSunSet <cloverchen.042@gmail.com>
2024-06-06 23:41:31 +0800

    Feat,intergrate all service to single file.

--- app.py
+++ app.py
@@ -2,9 +2,13 @@ from flask import Flask,request
 from flask_cors import CORS
 from controllers.homeController import home, saveImage
 import model.clients.gcsClient;
+from model.clients.sftpClient import sftpConnect,getDir,toCloseAllConnect;
 
+sftpConnect()
+# getDir()
+# toCloseAllConnect()
 app = Flask(__name__)
-CORS(app, origins=["http://localhost:8080"])
+CORS(app, origins=["http://localhost:8081"])
 app.add_url_rule('/home', 'home', home, methods=['GET'])
 app.add_url_rule('/saveImage', 'saveImage', saveImage, methods=['POST'])
 
