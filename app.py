e3a439b9620dd97cb441f9802802026c3d96a115
JustSleepTilSunSet <cloverchen.042@gmail.com>
2024-06-12 21:18:32 +0800

    Feat, a template is for upload image to sftp.

--- app.py
+++ app.py
@@ -2,11 +2,10 @@ from flask import Flask,request
 from flask_cors import CORS
 from controllers.homeController import home, saveImage
 import model.clients.gcsClient;
-from model.clients.sftpClient import sftpConnect,getDir,toCloseAllConnect;
+from model.clients.sftpClient import sftpConnect,getDir;
 
 sftpConnect()
-# getDir()
-# toCloseAllConnect()
+getDir()
 app = Flask(__name__)
 CORS(app, origins=["http://localhost:8081"])
 app.add_url_rule('/home', 'home', home, methods=['GET'])
