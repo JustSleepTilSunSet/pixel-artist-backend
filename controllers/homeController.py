ea4c52a22d1b8cb646bbe33de971e9bbd8cc86e4
JustSleepTilSunSet <cloverchen.042@gmail.com>
2024-06-15 23:10:06 +0800

    Feat, add login api.

--- controllers/homeController.py
+++ controllers/homeController.py
@@ -2,6 +2,9 @@ from flask import Flask, jsonify,request
 from model.Constants import status
 import base64
 from model.clients.sftpClient import uploadFile;
+from dotenv import dotenv_values
+import os
+
 def home():
     return jsonify({'message': 'hola'})
 
@@ -13,6 +16,21 @@ def saveImage():
         with open("q1.jpeg","wb") as f:
             f.write(file_content)
         uploadFile(file_content,'./share/testing.jpeg')
+    except Exception as e:
+        print(str(e))
+
+    return jsonify({'status': status["SUCCESS"], 'message': 'Hello, world!'})
+
+def login():
+    resp = request.json
+    try:
+        print(jsonify(resp))
+        isVaild = resp["account"] == os.getenv("TESTING_USER_ACCOUNT") and resp["pwd"] == os.getenv("TESTING_USER_PWD")
+        if isVaild !=True:
+            return jsonify({'status': status["FAIL"], 'message': 'login failed.'})
+        
+        print(isVaild);
+        return jsonify({'status': status["SUCCESS"], 'message': 'login success'})
     except Exception as e:
         print(str(e))
 
