39c9028f277930e16fe48f9e836d8ecce5c47e4c
JustSleepTilSunSet <cloverchen.042@gmail.com>
2024-06-16 11:56:49 +0800

    Feat,add login by guest api.

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask,request
 from flask_cors import CORS
-from controllers.homeController import home, saveImage, login
+from controllers.homeController import home, saveImage, login, loginByGuest
 import model.clients.gcsClient;
 from model.clients.sftpClient import sftpConnect,getDir;
 
@@ -11,6 +11,7 @@ CORS(app, origins=["http://localhost:8081"])
 app.add_url_rule('/home', 'home', home, methods=['GET'])
 app.add_url_rule('/saveImage', 'saveImage', saveImage, methods=['POST'])
 app.add_url_rule('/login', 'login', login, methods=['POST'])
+app.add_url_rule('/loginByGuest', 'loginByGuest', loginByGuest, methods=['POST'])
 
 @app.route("/")
 def hello_world():
