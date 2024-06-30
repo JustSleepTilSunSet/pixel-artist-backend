83ee3edaa629a8c0f1391d30958b2cc78b4e9897
JustSleepTilSunSet <cloverchen.042@gmail.com>
2024-06-30 15:39:22 +0800

    Feat, add loginbyguest.

--- model/database/initdb.py
+++ model/database/initdb.py
@@ -1,7 +1,12 @@
-
 from sqlalchemy import create_engine
 import os
 POSTGRES_URI = os.getenv("POSTGRES_URI")
-engine = create_engine(POSTGRES_URI,echo=True)
-engine.connect();
-print("Database connected.")
+def connectInitDatabase():
+    try:
+        engine = create_engine(POSTGRES_URI,echo=True)
+        engine.connect();
+        print("Database connected.")
+        return engine
+    except Exception as e :
+        print(f"An error occurred: {e}")
+        raise
