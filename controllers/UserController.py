bf122010f920bba8e7049706452b148628b0ede8
JustSleepTilSunSet <cloverchen.042@gmail.com>
2024-10-30 18:53:05 +0800

    Feat. add jwt token to response when the user logined the application.

--- controllers/UserController.py
+++ controllers/UserController.py
@@ -5,28 +5,30 @@ from model.Secret import Secret;
 from sqlalchemy.orm import sessionmaker
 from model.database.repository.UsersRepository import User
 from model.database.initdb import connectInitDatabase,createSession;
-
+from flask_jwt_extended import create_access_token,decode_token
+from datetime import timedelta
+import json
 def login():
     resp = request.json
     try:
         print(jsonify(resp))
         connectInitDatabase();
         session = createSession()
-        isVaild = session.query(User).filter_by(account = resp["account"]).first();
-
-        if isVaild:
-            print("User exists.")
-        else:
+        isVaild = (session.query(User).filter_by(account = resp["account"]).first()) is not None;
+        print("isVaild: ",isVaild);
+        print(isVaild is not True)
+        if isVaild is not True:
             print("User does not exist.")
             return jsonify({'status': status["FAIL"], 'message': 'login failed.'})
+        expires = timedelta(minutes=int(os.getenv("JWT_ACCESS_TOKEN_EXPIRES_MINUTES")))
+        access_token = create_access_token(identity=json.dumps(resp), expires_delta=expires);
 
-        print(isVaild);
-        return jsonify({'status': status["SUCCESS"], 'message': 'login success'})
+        return jsonify({'status': status["SUCCESS"], 'message': 'login success',"access_token": access_token})
     except Exception as e:
         print(str(e))
         return jsonify({'status': status["FAIL"], 'message': 'login failed.'})
 
-def loginByGuest():
+def loginByGuest(): 
     try:
         connectInitDatabase();
         session = createSession()
