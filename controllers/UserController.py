35c1f9616bd5abeb309ea143cde3cce44b25a7a5
JustSleepTilSunSet <cloverchen.042@gmail.com>
2024-11-03 20:46:43 +0800

    Updated. simplify logic when the user logined.

--- controllers/UserController.py
+++ controllers/UserController.py
@@ -2,7 +2,7 @@ from flask import Flask, jsonify,request
 from model.Constants import status
 import os
 from model.Secret import Secret;
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy import select
 from model.database.repository.UsersRepository import User
 from model.database.initdb import connectInitDatabase,createSession;
 from flask_jwt_extended import create_access_token, decode_token
@@ -12,34 +12,29 @@ def login():
         
     try:
         # Check the header exist token.
-        isRegistered = True;
-        userInfo = None;
         connectInitDatabase();
         session = createSession();
+        resp = request.json;
+        stmt = select(User).where(User.account == resp["account"])
+        result = session.execute(stmt)
+        userInfo = result.scalars().first();
+        if userInfo is None:
+            try:
+                session.add(User(account = resp["account"],password = resp["pwd"]));
+                session.commit();
+                userInfo = resp;
+            except Exception as e:
+                session.rollback();
+        else:
+            print(resp["pwd"])
+            print(userInfo.password);
+            if resp["pwd"] != userInfo.password:
+                return jsonify({'status': status["FAIL"], 'message': 'login failed',"access_token": None});
 
-        if request.headers.get('Authorization') is not None:
-            token_type, access_token = request.headers.get('Authorization').split(' ');
-            if token_type == os.getenv("TOKEN_FORMAT"):
-                try:
-                    userInfo = decode_token(access_token)["sub"];
-                    # parse user info.
-                    # NOTICE: Decode and get the user account info.
-                    print(userInfo);
-                    isRegistered = False;
-                except Exception as e:
-                    print(str(e));
-
-        if isRegistered:
-            resp = request.json;
-            # Inserted user setting.
-            session.add(User(account = resp["account"],password = resp["pwd"]));
-            session.commit();
-
-            # Generate token.
-            expires = timedelta(minutes=int(os.getenv("JWT_ACCESS_TOKEN_EXPIRES_MINUTES")))
-            access_token = create_access_token(identity=json.dumps(resp), expires_delta=expires);
-            userInfo = decode_token(access_token)["sub"];
-
+        # Generate token.
+        expires = timedelta(minutes=int(os.getenv("JWT_ACCESS_TOKEN_EXPIRES_MINUTES")))
+        access_token = create_access_token(identity=json.dumps(resp), expires_delta=expires);
+        # userInfo = decode_token(access_token)["sub"];
         return jsonify({'status': status["SUCCESS"], 'message': 'login success',"access_token": access_token})
     except Exception as e:
         print(str(e))
@@ -56,10 +51,16 @@ def loginByGuest():
         new_user = User(account=account, password= password)
         session.add(new_user)
         session.commit()
+     
+
+        # Generate token.
+        expires = timedelta(minutes=int(os.getenv("JWT_ACCESS_TOKEN_EXPIRES_MINUTES")))
+        access_token = create_access_token(identity=json.dumps(resp), expires_delta=expires);
 
         return jsonify({'status': status["SUCCESS"], 'message': {
-            "account":account,
-            "password":password
+            "account": account,
+            "password": password,
+            "access_token": access_token
         }})
     except Exception as e:
         print(str(e))
