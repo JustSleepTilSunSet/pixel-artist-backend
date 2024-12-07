4a1337bc78f8c47a4c0ceca2d242c0144af446ec
JustSleepTilSunSet <cloverchen.042@gmail.com>
2024-12-07 22:43:11 +0800

    Feat: implement gallery  apis.

--- controllers/PaintingController.py
+++ controllers/PaintingController.py
@@ -4,9 +4,11 @@ from model.clients.sftpClient import uploadFile;
 from flask_jwt_extended import decode_token
 from model.database.initdb import connectInitDatabase,createSession;
 from model.database.repository.PaintingRepository import Painting
+from model.clients.sftpClient import getSFTPFile;
 import hashlib
 import time
 import json
+import base64
 
 # NOTICE: 預檢類型?前端上傳畫作的類型是否唯一？
 def saveImage():
@@ -16,6 +18,7 @@ def saveImage():
     userToken = request.headers.get('Authorization');
     token = userToken.split('Bearer')[1].strip();
     account = None
+
     try:
         # Check the header exist token.
         connectInitDatabase();
@@ -26,7 +29,6 @@ def saveImage():
         # FileStorage一但使用完save或read,cache就會清空，所以這兩行只能二擇一
         # data.save('hi.jpeg'); #FileStorge 可以直接打save
         file_bytes = data.read();
-        # print(file_bytes);
         # 獲取當前時間戳
         timestamp = str(time.time())
 
@@ -36,8 +38,10 @@ def saveImage():
         print("account:", account);
         print("hash_name:", hash_name);
         fileName = f'{hash_name}.jpeg';
+        paintingPath = f'./share/{fileName}';
+        print(paintingPath);
         uploadFile(file_bytes,f'./share/{fileName}');
-        session.add(Painting(account = account,paintingName = fileName,customName=customName,paintingDescription=paintingDescription ));
+        session.add(Painting(account = account,paintingName = fileName,customName=customName,paintingDescription=paintingDescription,paintingPath=paintingPath));
         session.commit();
 
     except Exception as e:
@@ -67,8 +71,7 @@ def listImageById():
         paintingRawResult = session.query(Painting).filter_by(account=account).all();
         paintingResult = [row.to_dict() for row in paintingRawResult]  # 假設每行有 to_dict 方法
         paintingCount = session.query(Painting).filter_by(account=account).count();
-        # print(result[0].account);
-        # print(count);
+
     except Exception as e:
         print(str(e));
         return jsonify({'status': status["LOGIN_FAIL"]})
@@ -78,3 +81,16 @@ def listImageById():
             'paintingResult': paintingResult,
             'paintingCount': paintingCount
         }})
+
+def getImageToBase64():
+    try:
+        body = request.json;
+        # print(body["account"]);
+        print('paintingPath: ',body["paintingPath"]);
+        paintingData = getSFTPFile(remotePath=body["paintingPath"]);
+        # print(base64.b64encode(paintingData).decode('utf-8'));
+        return jsonify({'status': status["SUCCESS"],'image': base64.b64encode(paintingData).decode('utf-8')})
+    except Exception as e:
+        print("An exception occured: ",str(e));
+        return jsonify({'status': status["LOGIN_FAIL"]})
+    
